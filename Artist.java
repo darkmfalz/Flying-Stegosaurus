28478a2bbfb5f716ede6cc649c9ca104bdbce36e
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-08-19 19:35:51 -0400

    Initial commit. Added Artist and Commander.

---
+++ Artist.java
@@ -0,0 +1,7 @@
+package flying.stegosaurus;
+
+import javax.swing.JPanel;
+
+public class Artist extends JPanel{
+
+}
---
+++ Commander.java
@@ -0,0 +1,31 @@
+package flying.stegosaurus;
+
+import javax.swing.JFrame;
+
+public class Commander {
+	
+	public static JFrame frame;
+	public static Artist artist;
+	
+	public static void main(String[] args) throws Throwable{
+		
+		//Create the frame
+		frame = new JFrame();
+		//Create the artist
+		artist = new Artist();
+		//Add the artist to the frame
+		frame.getContentPane().add(artist);
+		//Make the frame closeable
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Packs the frame so everything fits
+		frame.pack();
+		//Sets the frame as resizeable
+		frame.setResizable(true);
+		//Centers the frame
+		frame.setLocationRelativeTo(null);
+		//Makes the frame visible
+		frame.setVisible(true);
+		
+	}
+	
+}
